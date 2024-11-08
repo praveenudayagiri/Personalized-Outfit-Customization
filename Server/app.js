d0bbf929a6b1db0fdb86a9efa95fafc9dc792fbb
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2024-11-08 20:33:39 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -12,6 +12,16 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"] // Allowed headers
 }));
 
+
+app.options('*', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://personalized-outfit-customization-rc6u.vercel.app");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.status(200).end(); // OK status
+});
+
+
 app.use(express.json());
 app.use(cookieParser());
 
