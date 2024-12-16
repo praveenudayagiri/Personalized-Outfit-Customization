0b1488e45a708416db018987609f1fd98d528c16
praveenudayagiri <168966600+praveenudayagiri@users.noreply.github.com>
2024-12-16 10:36:02 +0530

    Update database.js

--- Server/config/database.js
+++ Server/config/database.js
@@ -5,9 +5,12 @@ const connectDB = () => {
     mongoose.connect(
     "mongodb+srv://praveenudayagiri724:PRAVEEN@cluster0.lvusg.mongodb.net/praveendb?retryWrites=true&w=majority&appName=Cluster0"
 )
-.then(() => {
-    console.log('MongoDB connected successfully');
-})
+.then(() =>{
+    console.log("Database connection established...");
+    app.listen(9000,()  => {
+        console.log("server is sucessfully listening on port 9000....");
+    });
+}) 
 .catch(err => {
     console.error('Error connecting to MongoDB:', err);
 });
